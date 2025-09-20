8dee5a726f9fef7da2c1f19edccf539181097fd1
Ernest Yeboah <yeboahernest246@gmail.com>
2025-09-20 13:05:12 +0000

    back to previous settings

--- backend/api_store/api_store/settings.py
+++ backend/api_store/api_store/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'NAME': 'amaetonfashionhouse',
         'USER': 'root',
         'PASSWORD': 'greatlibertyschool.',
-        'HOST':'https://amaetonstore.pythonanywhere.com',
+        'HOST':'localhost',
         'PORT':'3306',
     }
 }
